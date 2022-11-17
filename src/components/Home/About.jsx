ab0253ad32c90554e08bc372b2533a49a0f86a63
ElizabethChillcce <eli.chillcce.08@gmail.com>
2022-11-17 16:03:20 -0500

    Avanzando con el componente About y sus estilos

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -6,9 +6,9 @@ import "../Css/About.css";
 const About = () => {
   return (
     <>
-      <section className="hero">
+      <section className="hero container">
         <div className="container-image-about">
-
+         <div className="col">
         <h3 className="title-hero">SOBRE MÍ</h3>
           <div>
             <p>Soy Front-End developer, a partir de mi formación como developer en Laboratoria he podido desarrollar páginas interactivas que resuelven problemas reales. Me apasiona desarrollar productos enfocados en el usuario, con mucha curiosidad por aprender nuevas tecnologías, con conocimientos en metodología ágiles (SCRUM), experiencia de usuario, lista para recibir y dar feedback, solución de problemas, comunicación asertiva. Mi objetivo es especializarme y tener un perfil de Full Stack, estoy convencida de que el trabajo en equipo es la mejor forma de avanzar en cualquier proyecto.</p>
@@ -16,16 +16,17 @@ const About = () => {
           <button type="button" className="container-btn">
                 <a className="btn-cv" href="" download target="_blank">DESCARGAR CV </a>
           </button>
-          <div className="container-image">
+          </div>
+          <div className="col-md-3 container-image">
             <img src={Eli} alt="Foto Elizabeth Ch" className="image-hero"/>
           </div>
 
           </div>
           <div className="col container-networks-social">
-            <div>
+            <div className="social-icons">
             <a href="https://github.com/ElizabethChillcce" target="_blank" rel="noreferrer"><FaGithub/></a>
             </div>
-            <div>
+            <div className="social-icons">
             <a href="https://www.linkedin.com/in/elizabeth-chillcce-95b13523b/" target="_blank" rel="noreferrer"><FaLinkedin/></a>
             </div>
         </div>
